f6980e0489f7796adcd20d8f5e6a165694fdef98
Letícia <leoliveira994@gmail.com>
2021-06-13 19:59:29 -0300

    Comentários

---
+++ fundaments/comment.js
@@ -0,0 +1,17 @@
+// comentário de uma linha
+console.log('Linha 1')
+
+/*
+  Comentário
+  de
+  várias
+  linhas
+*/
+console.log('Linha 2')
+
+/* 
+  * Comentário
+  * Comentário
+  * Cometário
+*/
+console.log('Linha 3')
