149d7bc94840ca1c2169f947666a5123dfa93aec
Letícia <leoliveira994@gmail.com>
2021-07-22 21:17:15 -0300

    variáveis e constantes

---
+++ fundaments/variables_and_constants.js
@@ -0,0 +1,28 @@
+
+// variáveis
+
+// var nome_da_variavel = valor
+
+// var não é recomendado
+
+// let nome_da_variavel = valor
+
+// o let é uma forma mais moderna de criar variáveis
+
+// o let não permite que você redefina o valor da variável
+
+var a = 2
+var a = 3
+let b = 1
+let b = 2
+
+console.log(a, b) // levanta erro
+// SyntaxError Identifier 'b' has already been declared
+
+// constantes 
+
+// valor que não poderá ser alterado
+
+const c = 5
+
+c = 5 // levanta erro
