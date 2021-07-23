8e97bcfb254440256f58f3e97a5c42f9ce4da7db
Letícia <leoliveira994@gmail.com>
2021-07-22 21:17:32 -0300

    tipagem fraca

---
+++ fundaments/tipagem_fraca.js
@@ -0,0 +1,11 @@
+let variable = 'Nice'
+
+console.log(variable)
+console.log(typeof variable) //type: string
+
+variable = 552
+
+console.log(variable)
+console.log(typeof variable) //type: number
+
+// tipagem fraca => no javascript, as variáveis podem ter vários valores...
