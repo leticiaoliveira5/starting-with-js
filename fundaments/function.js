dc333e0d1426b90f9300abdd2d03322933642266
leticiaoliveira5 <leticia.oliveira@vindi.com.br>
2021-08-05 14:11:26 -0300

    function... pt 2

--- fundaments/function.js
+++ fundaments/function.js
@@ -10,3 +10,25 @@ function soma(a, b = 0) {
 
 console.log(soma(5)) // 5
 console.log(soma(5,3)) // 8
+
+// função armazenada em variável
+
+const imprimeSoma = function (a, b) {
+  console.log(a + b)
+}
+
+imprimeSoma(1, 2) // 3
+
+// função arrow em variável
+
+const Soma = (a, b) => {
+  console.log(a + b)
+}
+
+Soma(1, 3) // 4
+
+// retorno implícito
+
+const subtracao = (a, b) => a - b
+
+console.log(subtracao(5, 3)) // 2
