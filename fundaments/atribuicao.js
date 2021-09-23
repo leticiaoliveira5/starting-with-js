a83f4d86c656e54f5526a408ccfac63acf240a68
leticiaoliveira5 <olivaleticia@hotmail.com>
2021-09-23 20:03:32 -0300

    atribuicao

---
+++ fundaments/atribuicao.js
@@ -0,0 +1,23 @@
+const a = 1
+let b = 2
+
+b += a // b = b + a
+// acrescenta o valor de a ao valor de b
+
+console.log(b) // 3
+
+b -= 2 // b = b - 2
+
+console.log(b) // 1
+
+b *= 10 // b = b * 10
+
+console.log(b) // 10
+
+b /= 2  // b = b / 2
+
+console.log(b) // 5
+
+b %= 2 // b = b % 2
+
+console.log(b) // 1
