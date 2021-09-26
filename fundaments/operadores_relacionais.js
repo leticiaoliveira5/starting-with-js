62f8c3d1aab1f4b1986efa07628e8cd6a9de2fce
leticiaoliveira5 <olivaleticia@hotmail.com>
2021-09-25 21:16:39 -0300

    Operadores relacionais

---
+++ fundaments/operadores_relacionais.js
@@ -0,0 +1,19 @@
+// igual/ estritamente igual
+console.log('1' == 1) // true // compara valores
+console.log('1' === 1) // false // estritamente diferente (tipo)
+
+// diferente / estritamente diferente
+console.log('3' != 3) // false
+console.log('3' !== 3) // true
+
+// maior/menor que
+console.log(3 < 2) // false
+console.log(3 > 2) // true
+
+// maior/menor ou igual
+console.log(3 >= 2) // true
+console.log(3 <= 2) // false
+
+
+console.log(undefined == null) // true
+console.log(undefined === null) // false
