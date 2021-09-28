0c572940bb50c00d07c2fa2208cfa5c88eeb6e55
leticiaoliveira5 <olivaleticia@hotmail.com>
2021-09-28 10:38:39 -0300

    operadores unarios e ternarios

---
+++ fundaments/operadores_ternarios.js
@@ -0,0 +1,6 @@
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado' // estrutura if else
+
+// condicao ? se for verdadeira : se for falsa
+
+console.log(resultado(7.1)) // Aprovado
+console.log(resultado(6.8)) // Reprovado
---
+++ fundaments/operadores_unarios.js
@@ -0,0 +1,14 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // soma 1 ao numero
+console.log(num1) // num1 = 2
+
+--num1 // subtrai 1 do número (2-1)
+console.log(num1) // 1
+
+console.log('num1 =', num1) // 1
+console.log('num2 =', num2) // 2
+
+console.log(++num1 === num2) // (2 === 2) true (só depois da comparação é substraído)
+
