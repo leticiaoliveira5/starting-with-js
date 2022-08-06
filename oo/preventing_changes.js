d53a711883e8e0800d0e23100c94753b05352385
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-05 22:32:30 -0300

    object trivia

---
+++ oo/preventing_changes.js
@@ -0,0 +1,8 @@
+// Object.preventExtenstions
+// não pode adicionar novos atributos, mas pode excluir ou modificar os atributos existentes
+
+// Object.seal
+// não pode adicionar nem deletar novos atributos, ainda é possível alterar os valores
+
+// Object.freeze = selado + valores constantes
+// não pode ser editado de nenhuma maneira, todos atributos tem o writable: false
