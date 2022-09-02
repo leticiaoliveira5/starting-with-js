71f586a6b96b81af4176106923fe8a9e10fcf205
leticiaoliveira5 <leoliveira994@gmail.com>
2022-09-02 19:35:00 -0300

    add database

--- node/api/src/server.js
+++ node/api/src/server.js
@@ -1,14 +1,33 @@
 const port = 3003
 
-const { response } = require('express')
 const express = require('express')
 const app = express()
+const db = require('./db.js')
 
 app.get('/produtos', (req, res, next) => {
-  response.send({ nome: 'Notebook', preco: 123.45 }) // convert to json
+  res.send({ nome: 'Notebook', preco: 123.45 }) // convert to json
 })
+
+app.get('/produtos', (req, res, next) => {
+  res.send(db.getProducts) // convert to json
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+  res.send(db.getProduct(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+  const product = db.saveProduct({
+    name: req.body.name,
+    price: req.body.price
+  })
+  res.send(product)
+})
+
+app.use((req, res, next) => {
+  res.send({ nome: 'Notebook', preco: 123.45 }) // convert to json
+}) // use any URL
 
 app.listen(port, () => {
   console.log("Servidor executando na porta" + port)
-}
-)
+})
